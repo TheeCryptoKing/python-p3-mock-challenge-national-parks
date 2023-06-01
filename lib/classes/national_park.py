8aa6bb7237127c5876947a50ac417e7a4c2a2a5b
kwamebrowne <theeecryptoking@gmail.com>
2023-06-01 00:28:58 -0500

    pair attempt

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -11,40 +11,50 @@ class NationalPark:
     
     @name.setter
     def name(self, name):
-        if isinstance(name, str) and not hasattr(self,'name') and 1 <= len(name) <= 15:
+        if isinstance(name, str) and not hasattr(self, 'name'):
             self._name = name
         else:
-            raise Exception 
-
+            raise Exception
+        
     def trips(self, new_trip=None):
         from classes.trip import Trip
         if new_trip and isinstance(new_trip, Trip):
             self._trips.append(new_trip)
-        return self._trips 
-        pass
-        
-    def visitors(self, new_visitor=None):
-        from classes.visitor import Visitor
-        if new_visitor not in self._visitors and isinstance(new_visitor, Visitor):
-            self._visitors.append(new_visitor)
-        return self._visitors 
+        return self._trips
         pass
     
+    def visitors(self, new_visitor=None):
+        from classes.visitor import Visitor
+        if new_visitor and isinstance(new_visitor, Visitor):
+            self._visitors.append(new_visitor)
+        return list(set(self._visitors))
+        pass
+    
+    
     def total_visits(self):
-        # trips give you amount of times park is visited
         return len(self._trips)
+        # return len(self._visitors)
         pass
     
     def best_visitor(self):
-        # micheals code
-        # return max(set(self._visitors), key=self._visitors.count)
-        visitor = None
-        visit_num = 0 
-        for v in self._visitors:
-            visits =  self._visitors.count(v)
-            if visits > visit_num:
-                visitor = v
-                visit_num = visits
-        return visitor 
-    # rabs main from the for loop 
-        pass
+        return max(set(self._visitors), key=self._visitors.count)
+        # pass
+        # visitor = None
+        # visits = 0
+        # for v in self._visitors:
+        #     if self._visitors.count(v) > visits:
+        #         visitor = v
+        #         visits =  self._visitors.count(v)
+        #     return visitor
+        
+        # for v in self._trips:
+        #     # self._trips.visitor
+        #     if self._trips.count(v) > visits:
+        #         visitor = v.visitor
+        #         visits = self._trips.count(v)
+        #     return visitor
+
+    # Syntax of sorted() 
+    #  sorted(self.trip, key=lambda index: index.end_date)
+    # Syntax of sort()
+    #  list.sort(key=lambda index, reverse)
